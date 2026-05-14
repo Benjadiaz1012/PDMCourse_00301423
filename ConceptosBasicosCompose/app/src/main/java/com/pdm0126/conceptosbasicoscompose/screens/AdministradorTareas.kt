5cd634be0571b3b93f9f1868c3127d5768e458b7
Oscar Diaz <00301423@uca.edu.sv>
2026-05-14 16:46:46 -0600

    Add: Agregar ConceptosBasicosCompose

--- ConceptosBasicosCompose/app/src/main/java/com/pdm0126/conceptosbasicoscompose/screens/AdministradorTareas.kt
+++ ConceptosBasicosCompose/app/src/main/java/com/pdm0126/conceptosbasicoscompose/screens/AdministradorTareas.kt
@@ -1,9 +1,62 @@
 package com.pdm0126.conceptosbasicoscompose.screens
 
+import android.R.attr.fontWeight
+import android.R.id.bold
+import android.support.v4.os.IResultReceiver2
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.pdm0126.conceptosbasicoscompose.R
+import kotlin.io.encoding.Base64
+import kotlin.random.Random
 
 
 @Composable
 fun adminTarea(onNext: () -> Unit) {
-
+    Scaffold { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ImageCheck(modifier = Modifier)
+            Text(
+                text = stringResource(R.string.all_tasks_completed),
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(top = 24.dp, bottom = 8.dp),
+            )
+            Text(
+                text = stringResource(R.string.nice_work),
+                modifier = Modifier.padding(bottom = 10.dp)
+            )
+            Button(onClick = onNext) { Text(text = "Avanzar") }
+        }
     }
+}
+
+@Composable
+fun ImageCheck(modifier: Modifier) {
+    val check = painterResource(R.drawable.ic_task_completed)
+    Image(
+        painter = check,
+        contentDescription = null,
+        modifier = Modifier,
+    )
+}
--- ConceptosBasicosCompose/app/src/main/java/com/pdm0126/conceptosbasicoscompose/screens/CuadranteCompose.kt
+++ ConceptosBasicosCompose/app/src/main/java/com/pdm0126/conceptosbasicoscompose/screens/CuadranteCompose.kt
@@ -1,8 +1,142 @@
 package com.pdm0126.conceptosbasicoscompose.screens
 
+import android.text.Layout
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.TextAutoSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.unit.dp
+import com.pdm0126.conceptosbasicoscompose.R
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 
 @Composable
-fun CuandranteCompose(onNext: () -> Unit){
-
+fun CuandranteCompose(onNext: () -> Unit) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .background(Color(0xFFEADDFF))
+                        .weight(1f)
+                        .padding(16.dp), contentAlignment = Alignment.Center
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = stringResource(R.string.text_composable),
+                            color = Color.Black,
+                            fontWeight = FontWeight.Bold,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.padding(bottom = 16.dp)
+                        )
+                        Text(
+                            text = stringResource(R.string.MaterialDesign),
+                            color = Color.Black, textAlign = TextAlign.Justify
+                        )
+                    }
+                }
+                Box(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .background(Color(0xFFD0BCFF))
+                        .weight(1f)
+                        .padding(16.dp), contentAlignment = Alignment.Center
+                ) {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Text(
+                            text = stringResource(R.string.image_composable),
+                            color = Color.Black,
+                            fontWeight = FontWeight.Bold,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.padding(bottom = 16.dp)
+                        )
+                        Text(
+                            text = stringResource(R.string.Painter),
+                            color = Color.Black,
+                            textAlign = TextAlign.Justify
+                        )
+                    }
+                }
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .background(Color(0xFFB69DF8))
+                        .weight(1f)
+                        .padding(16.dp), contentAlignment = Alignment.Center
+                ) {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Text(
+                            text = stringResource(R.string.row_composable),
+                            textAlign = TextAlign.Center,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.Black,
+                            modifier = Modifier.padding(bottom = 16.dp)
+                        )
+                        Text(
+                            text = stringResource(R.string.LayoutComposable),
+                            textAlign = TextAlign.Justify,
+                            color = Color.Black
+                        )
+                    }
+                }
+                Box(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .background(Color(0xFFF6EDFF))
+                        .weight(1f)
+                        .padding(16.dp), contentAlignment = Alignment.Center
+                ) {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Text(
+                            text = stringResource(R.string.column_composable),
+                            textAlign = TextAlign.Center,
+                            color = Color.Black,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier.padding(bottom = 16.dp)
+                        )
+                        Text(
+                            text = stringResource(R.string.VerticalLayout),
+                            textAlign = TextAlign.Justify,
+                            color = Color.Black
+                        )
+                    }
+                }
+            }
+        }
+        Button(
+            onClick = onNext,
+            modifier = Modifier.align(Alignment.Center)
+        ) { Text(text = "Regresar a Home") }
+    }
 }
